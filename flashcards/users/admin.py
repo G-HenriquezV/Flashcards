93ffb85daa86286e379db3378825a4ebce54f5f7
German Henriquez <germanandres.hv@gmail.com>
2020-08-05 15:56:23 +0000

    User admin added

--- flashcards/users/admin.py
+++ flashcards/users/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from flashcards.leitner.models import Deck, Card, Box
 from flashcards.users.models import User
 from flashcards.notes.models import Note
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Note)
 admin.site.register(Deck)
 admin.site.register(Card)
